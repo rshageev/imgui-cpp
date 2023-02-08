3f8288b7d587a9be1607c9866f5edc0c3dbcc96d
Renat Shageev <shageev.renat@gmail.com>
2023-02-08 23:40:39 +0000

    Fixed some unitialized data

--- imgui.h
+++ imgui.h
@@ -1895,10 +1895,10 @@ struct ImGuiStyle
 // If prior to 1.87 you used io.KeysDownDuration[] (which was marked as internal), you should use GetKeyData(key)->DownDuration and *NOT* io.KeysData[key]->DownDuration.
 struct ImGuiKeyData
 {
-    bool        Down;               // True for if key is down
-    float       DownDuration;       // Duration the key has been down (<0.0f: not pressed, 0.0f: just pressed, >0.0f: time held)
-    float       DownDurationPrev;   // Last frame duration the key has been down
-    float       AnalogValue;        // 0.0f..1.0f for gamepad values
+    bool Down = false;               // True for if key is down
+    float DownDuration = 0.0f;       // Duration the key has been down (<0.0f: not pressed, 0.0f: just pressed, >0.0f: time held)
+    float DownDurationPrev = 0.0f;   // Last frame duration the key has been down
+    float AnalogValue = 0.0f;        // 0.0f..1.0f for gamepad values
 };
 
 struct ImGuiIO
